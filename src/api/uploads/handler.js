1dae49e9e319e947849c5f8df8974207259c3f05
ahmdhndr <achmadhendarsyah7@yahoo.com>
2021-09-07 15:52:18 +0700

    S3 storage implementend

--- src/api/uploads/handler.js
+++ src/api/uploads/handler.js
@@ -12,14 +12,12 @@ class UploadsHandler {
     try {
       // dapatkan data pd request.payload yg mrpkn Readable
       const { data } = request.payload;
-      // eslint-disable-next-line no-underscore-dangle
-      console.log(data.hapi, data._data);
       // validasi nilai data.hapi.headers
       this._validator.validateImageHeaders(data.hapi.headers);
       // setelah proses validasi, lgsg tulis berkas yg dikirim pd storage
       // melalui fungsi writeFile pada this._service.writeFile
       // berikan data sbg parameter file dan data.hapi sbg parameter meta
-      const filename = await this._service.writeFile(data, data.hapi);
+      const fileLocation = await this._service.writeFile(data, data.hapi);
 
       // fungsi writeFile mengembalikan nama berkas (filename) yg ditulis
       // manfaatkan nama berkas ini dlm membuat nilai fileLocation
@@ -27,7 +25,7 @@ class UploadsHandler {
       const response = h.response({
         status: 'success',
         data: {
-          fileLocation: `http://${process.env.HOST}:${process.env.PORT}/upload/images/${filename}`,
+          fileLocation,
         },
       });
       response.code(201);
